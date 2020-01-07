0813329e29e703b17bbc99c28349d808e5410320
Ross <rosso5792@gmail.com>
2020-01-07 16:41:23 +0000

    paint mvp complete

--- models/paint_can.js
+++ models/paint_can.js
@@ -2,6 +2,17 @@ const Paint = function(litres){
   this.litres = litres;
 };
 
+Paint.prototype.checkEmpty = function(){
+  if (this.litres) {
+    return false;
+  }
+  else {
+    return true;
+  }
+};
 
+Paint.prototype.empty = function(){
+  this.litres = 0;
+};
 
 module.exports = Paint;
--- models/specs/paint_can_spec.js
+++ models/specs/paint_can_spec.js
@@ -13,10 +13,19 @@ describe('Paint', function(){
     const actual = paint.litres;
     assert.strictEqual(actual, 10);
   });
-
-
-
-
-
+  it('should know if its empty - false', function(){
+    const actual = paint.checkEmpty();
+    assert.strictEqual(actual, false);
+  });
+  it('should know if its empty - true', function(){
+    paint.empty();
+    const actual = paint.checkEmpty();
+    assert.strictEqual(actual, true);
+  });
+  it('should be able to empty itself', function(){
+    paint.empty();
+    const actual = paint.litres;
+    assert.strictEqual(actual, 0);
+  });
 
 }); // describe close

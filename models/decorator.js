0757dfe378a17191a6a86fcec510128dc316e6fb
Ross <rosso5792@gmail.com>
2020-01-07 17:19:10 +0000

    mvp complete

--- models/decorator.js
+++ models/decorator.js
@@ -8,4 +8,26 @@ Decorator.prototype.addToStock = function(paint){
   this.paintStock.push(paint);
 };
 
+Decorator.prototype.totalLitres = function(){
+  total = 0;
+  for (paint of this.paintStock){
+    total += paint.litres
+  }
+  return total;
+};
+
+Decorator.prototype.hasEnoughPaint = function(room){
+ if (this.totalLitres() > room.area){
+   return true;
+ } else {
+   return false;
+ };
+};
+
+Decorator.prototype.paintRoom = function (room) {
+  if(this.hasEnoughPaint(room)){
+    room.updatePainted()
+  }
+};
+
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js');
 const Paint = require('../paint_can.js');
-
+const Room = require('../room.js');
 
 
 
@@ -9,10 +9,12 @@ describe('Decorator', function(){
 
   let decorator;
   let paint;
+  let room;
 
   beforeEach(function(){
     decorator = new Decorator();
     paint = new Paint(10);
+    room = new Room(14);
   });
 
   it('should start with no paint', function(){
@@ -20,12 +22,29 @@ describe('Decorator', function(){
     assert.strictEqual(actual, 0);
   });
   it('should be able to add a can of paint', function(){
-    decorator.addToStock(paint)
+    decorator.addToStock(paint);
     const actual = decorator.paintStock.length;
     assert.deepStrictEqual(actual, 1);
   });
-
-
+  it('should be able to calculate total litres of paint in stock', function(){
+    decorator.addToStock(paint);
+    decorator.addToStock(paint);
+    const actual = decorator.totalLitres();
+    assert.deepStrictEqual(actual, 20)
+  });
+  it('should be able to calculate if enough paint to paint room', function(){
+    decorator.addToStock(paint);
+    decorator.addToStock(paint);
+    const actual = decorator.hasEnoughPaint(room);
+    assert.strictEqual(actual, true);
+  });
+  it('should be able to paint room if enough stock', function(){
+    decorator.addToStock(paint);
+    decorator.addToStock(paint);
+    decorator.paintRoom(room)
+    const actual = room.painted
+    assert.strictEqual(actual, true)
+  });
 
 
 }) // decorator close
